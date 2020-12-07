e674e4c3ab09ba1f678a18cd50a27426e859e14b
ip613s07 <vyacheslavgorohov@mail.ru>
2020-12-07 20:03:38 +0700

    Add Drag and Drop

---
+++ 2DCardGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    Transform hand;
+    Transform canvas;
+
+    CanvasGroup canvasGroup;
+
+    private bool isDraggable = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        hand = transform.parent;
+        canvas = transform.parent.parent;
+
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        if (isDraggable)
+        {
+
+            transform.position = eventData.position;
+
+        }
+
+    }    
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        if (isDraggable)
+        {
+
+            transform.SetParent(canvas);
+            canvasGroup.blocksRaycasts = false;
+
+            //Add array of valid drop zones to hightlight them
+
+        }
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        if (isDraggable)
+        {
+
+            transform.SetParent(hand);
+            canvasGroup.blocksRaycasts = true;
+
+        }
+
+    }
+
+    public void PutOnField(Transform parent)
+    {
+
+        isDraggable = false;
+        transform.SetParent(parent);
+
+    }
+
+    public void PutInHand(Transform parent)
+    {
+
+        transform.SetParent(parent);
+
+    }
+
+}
---
+++ 2DCardGame/Assets/Scripts/DropZone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+
+        if (draggable == null)
+        {
+
+            return;            
+
+        }
+
+        if (transform.tag == "Hand")
+        {
+
+            draggable.PutInHand(transform);
+
+            return;
+
+        }
+
+        draggable.PutOnField(transform);
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+}
